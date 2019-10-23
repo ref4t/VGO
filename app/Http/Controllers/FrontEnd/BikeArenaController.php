9fed9fb2b586b586a82179ec11723e45f4646a98
Fahim-Mazumder <41013788+Fahim-Mazumder@users.noreply.github.com>
2019-10-24 03:35:51 +0600

    Controller name changed
    BikeArena to BikeArenaController

--- app/Http/Controllers/FrontEnd/BikeArenaController.php
+++ app/Http/Controllers/FrontEnd/BikeArenaController.php
@@ -5,12 +5,10 @@ namespace App\Http\Controllers\FrontEnd;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 
-class BikeArena extends Controller
+class BikeArenaController extends Controller
 {
     public function index()
     {
-
-        return view('bikearena.index');
-
+        return view('bikeArena.blog');
     }
 }
